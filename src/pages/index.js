990c7832b7549b4017500cec979212b01d125f5f
Adriankf59 <adriancuman@gmail.com>
2024-12-18 01:20:27 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,7 +93,7 @@ export default function Home({ mountains, error }) {
           <div className="absolute inset-0 bg-black opacity-50"></div>
           <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">
-              Ready to do this?
+              Jalur Pendakian Gunung Nusantara
             </h1>
             <div className="w-full max-w-lg relative">
               <input
